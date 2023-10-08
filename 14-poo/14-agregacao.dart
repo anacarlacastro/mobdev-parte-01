cc83f1d057fbb668079f4b0da293f6a526579344
accastro <111244234+anacarlacastro@users.noreply.github.com>
2023-10-08 14:36:25 -0300

    Update 14-agregacao.dart

--- 14-poo/14-agregacao.dart
+++ 14-poo/14-agregacao.dart
@@ -7,6 +7,9 @@ class Dependente {
   Dependente(String nome) {
     this._nome = nome;
   }
+   Map<String, dynamic> toJson() {
+    return {"nome": _nome};
+}
 }
 
 class Funcionario {
@@ -17,6 +20,9 @@ class Funcionario {
     this._nome = nome;
     this._dependentes = dependentes;
   }
+   Map<String, dynamic> toJson() {
+    return {"nome": _nome, "dependentes": _dependentes.map((d) => d.toJson()).toList()};
+}
 }
 
 class EquipeProjeto {
@@ -27,16 +33,35 @@ class EquipeProjeto {
     _nomeProjeto = nomeprojeto;
     _funcionarios = funcionarios;
   }
+   Map<String, dynamic> toJson() {
+    return {"nomeProjeto": _nomeProjeto, "funcionarios": _funcionarios.map((f) => f.toJson()).toList()};
 }
 
 void main() {
   // 1. Criar varios objetos Dependentes
-  // 2. Criar varios objetos Funcionario
-  // 3. Associar os Dependentes criados aos respectivos
-  //    funcionarios
+  var dependente1 = Dependente("Dependente 1");
+  var dependente2 = Dependente("Dependente 2");
+  var dependente3 = Dependente("Dependente 3");
+  var dependente4 = Dependente("Dependente 4");
+  var dependente5 = Dependente("Dependente 5");
+
+  // 2. Criar varios objetos Funcionario  / 3. Associar os Dependentes criados aos respectivos funcionários
+  var funcionario1 = Funcionario("João", [dependente1]);
+  var funcionario2 = Funcionario("Ana", [dependente2]);
+  var funcionario3 = Funcionario("Linda", [dependente3]); 
+  var funcionario4 = Funcionario("Arthur", [dependente4]); 
+  var funcionario5 = Funcionario("Letícia", [dependente5]);
+
   // 4. Criar uma lista de Funcionarios
-  // 5. criar um objeto Equipe Projeto chamando o metodo
-  //    contrutor que da nome ao projeto e insere uma
-  //    coleção de funcionario
+  var funcionarios = [funcionario1, funcionario2, funcionario3, funcionario4, funcionario5];
+
+  // 5. criar um objeto Equipe Projeto chamando o metodo contrutor que da nome ao projeto e insere uma coleção de funcionario
+  var equipeProjeto = EquipeProjeto("Meu Projeto", funcionarios);
+  
   // 6. Printar no formato JSON o objeto Equipe Projeto.
+  equipeProjeto = EquipeProjeto("Meu Projeto", funcionarios);
+  var equipeProjetoJson = jsonEncode(equipeProjeto.toJson());
+  print(equipeProjetoJson);
+
+}
 }
